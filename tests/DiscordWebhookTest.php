30086f643cbd9d1a324de9d039cdb2e1c1530cbd
mhhidayat <mhhidayat811@gmail.com>
2025-12-06 08:59:48 +0700

    test: allow tts

--- tests/DiscordWebhookTest.php
+++ tests/DiscordWebhookTest.php
@@ -41,6 +41,7 @@ final class DiscordWebhookTest extends TestCase
             ->setWebhookURL("https://discord.com/api/webhooks/1446513684712132700/WwnU7-koive-XgBRu0lFDawVFzgoUqc-FzEOOKWxCy_IsHYGOLJMf3iix-Joib7KN1WT")
             ->setUsername("mhhidayat")
             ->setAvatar("https://pikaso.cdnpk.net/private/production/1786532689/upload.png?token=exp=1765238400~hmac=86e6cd793377406c0ed94aa9d01331085903861c075810b3e33304e3a2c841b4")
+            ->allowTTS()
             ->text("Hello test")
             ->send();
 
