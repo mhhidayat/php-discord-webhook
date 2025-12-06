531ef34783166805702813a112c9f39d20025b9c
mhhidayat <mhhidayat811@gmail.com>
2025-12-06 09:49:53 +0700

    feat: load env for private informationn

--- tests/DiscordWebhookTest.php
+++ tests/DiscordWebhookTest.php
@@ -1,16 +1,28 @@
 <?php
 
+use Dotenv\Dotenv;
 use Mhhidayat\PhpWebhookDiscord\DiscordWebhook;
 use PHPUnit\Framework\TestCase;
 
 final class DiscordWebhookTest extends TestCase
 {
+    private string $webhookURL, $avatarURL;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Dotenv::createImmutable(__DIR__)->load();
+
+        $this->webhookURL = $_ENV["TEST_WEBHOOK_URL"] ?? '';
+        $this->avatarURL  = $_ENV["TEST_AVATAR_URL"] ?? '';
+    }
+
     public function testWebhookSuccess()
     {
         $respDiscordWebhook = DiscordWebhook::withHeaders([
             "Content-Type: application/json",
         ])
-            ->setWebhookURL("https://discord.com/api/webhooks/1446513684712132700/WwnU7-koive-XgBRu0lFDawVFzgoUqc-FzEOOKWxCy_IsHYGOLJMf3iix-Joib7KN1WT")
+            ->setWebhookURL($this->webhookURL)
             ->setContent([
                 "content" => "Hello Test"
             ])
@@ -24,7 +36,7 @@ final class DiscordWebhookTest extends TestCase
         $respDiscordWebhook = DiscordWebhook::withHeaders([
             "Content-Type: application/json",
         ])
-            ->setWebhookURL("https://discord.com/api/webhooks/1446513684712132700/WwnU7-koive-XgBRu0lFDawVFzgoUqc-FzEOOKWxCy_IsHYGOLJMf3iix-Joib7KN1WT")
+            ->setWebhookURL($this->webhookURL)
             ->setContent([
                 "content" => "Hello Test"
             ])
@@ -40,9 +52,9 @@ final class DiscordWebhookTest extends TestCase
         $respDiscordWebhook = DiscordWebhook::withHeaders([
             "Content-Type: application/json",
         ])
-            ->setWebhookURL("https://discord.com/api/webhooks/1446513684712132700/WwnU7-koive-XgBRu0lFDawVFzgoUqc-FzEOOKWxCy_IsHYGOLJMf3iix-Joib7KN1WT")
+            ->setWebhookURL($this->webhookURL)
             ->setUsername("mhhidayat")
-            ->setAvatar("https://pikaso.cdnpk.net/private/production/1786532689/upload.png?token=exp=1765238400~hmac=86e6cd793377406c0ed94aa9d01331085903861c075810b3e33304e3a2c841b4")
+            ->setAvatar($this->avatarURL)
             ->allowTTS()
             ->text("Hello test")
             ->send();
