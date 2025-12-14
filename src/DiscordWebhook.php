b9a78812d1f31b264b5bb9b833cc41cce2a1e60c
mhhidayat <mhhidayat811@gmail.com>
2025-12-14 10:39:56 +0700

    refactor: implement trait discord client

--- src/DiscordWebhook.php
+++ src/DiscordWebhook.php
@@ -2,111 +2,11 @@
 
 namespace Mhhidayat\PhpDiscordClient;
 
-use Mhhidayat\PhpDiscordClient\Core\MainDiscordClient;
+use Mhhidayat\PhpDiscordClient\Core\CoreDiscordClient;
 use Mhhidayat\PhpDiscordClient\Interface\DiscordWebhookInterface;
+use Mhhidayat\PhpDiscordClient\Trait\HasDiscordClient;
 
-class DiscordWebhook implements DiscordWebhookInterface
+class DiscordWebhook extends CoreDiscordClient implements DiscordWebhookInterface
 {
-    private MainDiscordClient $client;
-
-    public function __construct()
-    {
-        $this->client = (new MainDiscordClient())
-            ->setClientTarget($this);
-    }
-
-    public static function make(): self
-    {
-        return new self();
-    }
-
-    public static function withConfig(array $config): self
-    {
-        $instance = new self();
-        $instance->client = MainDiscordClient::withConfig($config);
-        return $instance;
-    }
-
-    public static function withHeaders(array $headers): self
-    {
-        $instance = new self();
-        $instance->client = MainDiscordClient::withHeaders($headers);
-        return $instance;
-    }
-
-    public static function timeout(int $seconds): self
-    {
-        $instance = new self();
-        $instance->client = MainDiscordClient::timeout($seconds);
-        return $instance;
-    }
-
-    public function setWebhookURL(string $setWebhookURL): self
-    {
-        $this->client->setWebhookURL($setWebhookURL);
-        return $this;
-    }
-
-    public function setContent(array|\Closure $contentHandler): self
-    {
-        $this->client->setContent($contentHandler);
-        return $this;
-    }
-
-    public function addEmbeds(\Closure $embedsHandler): self
-    {
-        $this->client->addEmbeds($embedsHandler);
-        return $this;
-    }
-
-    public function text(string $text): self
-    {
-        $this->client->text($text);
-        return $this;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->client->setUsername($username);
-        return $this;
-    }
-
-    public function setAvatar(string $avatarURL): self
-    {
-        $this->client->setAvatar($avatarURL);
-        return $this;
-    }
-
-    public function allowTTS(): self
-    {
-        $this->client->allowTTS();
-        return $this;
-    }
-
-    public function send(): self
-    {
-        $this->client->send();
-        return $this;
-    }
-
-    public function sendWhen(bool|\Closure $isSendHandler): self
-    {
-        $this->client->sendWhen($isSendHandler);
-        return $this;
-    }
-
-    public function successful(): bool
-    {
-        return $this->client->successful();
-    }
-
-    public function failed(): bool
-    {
-        return $this->client->failed();
-    }
-
-    public function getResponseJson(): string
-    {
-        return $this->client->getResponseJson();
-    }
+    use HasDiscordClient;
 }
